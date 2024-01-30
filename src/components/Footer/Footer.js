fce01839170b591566601c404d7606125347d4d6
mirajhad <mirajhadish48@gmail.com>
2024-01-30 17:21:02 +0530

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,39 +4,39 @@ import logo from "../../assets/logo.png";
 const Footer = () => {
   return (
     <>
-      <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2024{" "}
-            <Link href="#" class="hover:underline">
-              cottonmouth
+       <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800 bottom-0 fixed w-full">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2024{" "}
+          <Link to="#" className="hover:underline">
+            cottonmouth
+          </Link>
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <Link to="#" className="hover:underline me-4 md:me-6">
+              About
             </Link>
-            . All Rights Reserved.
-          </span>
-          <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-            <li>
-              <Link to="#" class="hover:underline me-4 md:me-6">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link to="#" class="hover:underline me-4 md:me-6">
-                Privacy Policy
-              </Link>
-            </li>
-            <li>
-              <Link to="#" class="hover:underline me-4 md:me-6">
-                Licensing
-              </Link>
-            </li>
-            <li>
-              <Link to="#" class="hover:underline">
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </footer>
+          </li>
+          <li>
+            <Link to="#" className="hover:underline me-4 md:me-6">
+              Privacy Policy
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="hover:underline me-4 md:me-6">
+              Licensing
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="hover:underline">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
     </>
   );
 };
