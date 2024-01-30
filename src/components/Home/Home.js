83876e87a25e29910de6e019ad0e44ec5522d2c9
mirajhad <mirajhadish48@gmail.com>
2024-01-30 18:45:54 +0530

    style

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
 const Home = () => {
   return (
@@ -6,7 +7,7 @@ const Home = () => {
    <div className="home">
       <div className="header">
         <h1 className="text-4xl font-bold text-blue-500 mb-2">Welcome to Gani Tobacco</h1>
-        <p className="text-gray-600">Your go-to place for awesome React projects</p>
+        <p className="text-gray-600">Choose Life, Not Tobacco: Ignite Hope, Not Cancer.</p>
       </div>
       <div className="features flex space-x-4 mb-8">
         <div className="feature flex-1 p-4 border rounded-lg bg-white shadow-md">
@@ -17,16 +18,13 @@ const Home = () => {
           <h2 className="text-lg font-semibold mb-2">Responsive Layout</h2>
           <p>Works seamlessly on all devices</p>
         </div>
-        <div className="feature flex-1 p-4 border rounded-lg bg-white shadow-md">
-          <h2 className="text-lg font-semibold mb-2">Reusable Components</h2>
-          <p>Efficient and maintainable code</p>
-        </div>
+        
       </div>
       <div className="cta bg-gray-100 p-8 rounded-lg text-center">
-        <h2 className="text-2xl font-semibold mb-4">Get Started Today!</h2>
-        <button className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none">
-          Sign Up
-        </button>
+        <h2 className="text-2xl font-semibold mb-4">Book Your Order Today!</h2>
+        <Link to="/order" className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none">
+          Book Now
+        </Link>
       </div>
     </div>
   </>
--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,7 +3,7 @@ import React from "react";
 const Order = () => {
   return (
     <>
-      <section className=" order bg-white dark:bg-gray-900">
+      <section className="bg-white dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
           <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
             Order Now
