3fe01b90ab28de93db8d99886b3d97a49bd150c5
mirajhad <mirajhadish48@gmail.com>
2024-01-30 17:12:34 +0530

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,59 +3,41 @@ import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 const Footer = () => {
   return (
-    <div>
-      <footer className="bg-white rounded-lg shadow dark:bg-gray-900   fixed 
-             inset-x-0 
-             bottom-0">
-        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-          <div className="sm:flex sm:items-center sm:justify-between">
-            <Link
-              to="#"
-              className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
-            >
-              <img
-                src={logo}
-                className="h-8"
-                alt="Logo"
-              />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-               Gani Tobacco
-              </span>
-            </Link>
-            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-              <li>
-                <Link to="#" className="hover:underline me-4 md:me-6">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link to="#" className="hover:underline me-4 md:me-6">
-                  Privacy Policy
-                </Link>
-              </li>
-              <li>
-                <Link to="#" className="hover:underline me-4 md:me-6">
-                  Licensing
-                </Link>
-              </li>
-              <li>
-                <Link to="#" className="hover:underline">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023{" "}
-            <Link to="" className="hover:underline">
+    <>
+      <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2024{" "}
+            <Link href="#" class="hover:underline">
               cottonmouth
             </Link>
             . All Rights Reserved.
           </span>
+          <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+              <Link to="#" class="hover:underline me-4 md:me-6">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="#" class="hover:underline me-4 md:me-6">
+                Privacy Policy
+              </Link>
+            </li>
+            <li>
+              <Link to="#" class="hover:underline me-4 md:me-6">
+                Licensing
+              </Link>
+            </li>
+            <li>
+              <Link to="#" class="hover:underline">
+                Contact
+              </Link>
+            </li>
+          </ul>
         </div>
       </footer>
-    </div>
+    </>
   );
 };
 
