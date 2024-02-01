c274734f39f9b019d26dc6ca4ba28997a0760d12
mirajhad <mirajhadish48@gmail.com>
2024-02-01 20:14:15 +0530

    dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 const Dashboard = () => {
   return (
     <>
-     
       <div className="flex flex-col md:flex-row h-screen bg-gray-100">
         {/* Sidebar */}
         <div className="w-full md:w-64 bg-gray-800 text-white p-4">
@@ -33,21 +32,28 @@ const Dashboard = () => {
           <h2 className="text-2xl font-bold mb-4">Welcome to the Dashboard</h2>
           <div className="bg-white p-4 rounded shadow">
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
                     <th scope="col" className="px-6 py-3">
-                      Product name
+                      Name
                     </th>
                     <th scope="col" className="px-6 py-3">
-                      Color
+                      Email
                     </th>
                     <th scope="col" className="px-6 py-3">
-                      Category
+                      Address
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Phone
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Quantity
                     </th>
                     <th scope="col" className="px-6 py-3">
                       Price
                     </th>
+
                     <th scope="col" className="px-6 py-3">
                       <span className="sr-only">Edit</span>
                     </th>
@@ -55,113 +61,15 @@ const Dashboard = () => {
                 </thead>
                 <tbody>
                   <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      Apple MacBook Pro 17"
-                    </th>
+                    <td className="px-6 py-4">Silver</td>
                     <td className="px-6 py-4">Silver</td>
                     <td className="px-6 py-4">Laptop</td>
+                    <td className="px-6 py-4">$2999</td>
+                    <td className="px-6 py-4">$2999</td>
                     <td className="px-6 py-4">$2999</td>
                     <td className="px-6 py-4 text-right">
                       <Link
                         href="#"
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      >
-                        Edit
-                      </Link>
-                    </td>
-                  </tr>
-                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      Microsoft Surface Pro
-                    </th>
-                    <td className="px-6 py-4">White</td>
-                    <td className="px-6 py-4">Laptop PC</td>
-                    <td className="px-6 py-4">$1999</td>
-                    <td className="px-6 py-4 text-right">
-                      <Link
-                        to="#"
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      >
-                        Edit
-                      </Link>
-                    </td>
-                  </tr>
-                  <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      Magic Mouse 2
-                    </th>
-                    <td className="px-6 py-4">Black</td>
-                    <td className="px-6 py-4">Accessories</td>
-                    <td className="px-6 py-4">$99</td>
-                    <td className="px-6 py-4 text-right">
-                      <Link
-                        to="#"
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      >
-                        Edit
-                      </Link>
-                    </td>
-                  </tr>
-                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      Apple MacBook Pro 17"
-                    </th>
-                    <td className="px-6 py-4">Silver</td>
-                    <td className="px-6 py-4">Laptop</td>
-                    <td className="px-6 py-4">$2999</td>
-                    <td className="px-6 py-4 text-right">
-                      <Link
-                        href="#"
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      >
-                        Edit
-                      </Link>
-                    </td>
-                  </tr>
-                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      Microsoft Surface Pro
-                    </th>
-                    <td className="px-6 py-4">White</td>
-                    <td className="px-6 py-4">Laptop PC</td>
-                    <td className="px-6 py-4">$1999</td>
-                    <td className="px-6 py-4 text-right">
-                      <Link
-                        to="#"
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      >
-                        Edit
-                      </Link>
-                    </td>
-                  </tr>
-                  <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      Magic Mouse 2
-                    </th>
-                    <td className="px-6 py-4">Black</td>
-                    <td className="px-6 py-4">Accessories</td>
-                    <td className="px-6 py-4">$99</td>
-                    <td className="px-6 py-4 text-right">
-                      <Link
-                        to="#"
                         className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
                       >
                         Edit
