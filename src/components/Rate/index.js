6a5a22afd276f93462377a050ffd0c0d9c73282e
Nargis Fatime <nargis.fatima160@gmail.com>
2024-02-08 18:32:12 +0530

    rate ui changes

--- src/components/Rate/index.js
+++ src/components/Rate/index.js
@@ -5,17 +5,23 @@ const Rate = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const addProduct = async (data) => {
+    console.log(data)
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit((values) => addProduct(values))}>
         <div className="main">
           <div className="item">
             <label for="item">Item name:</label>
-            <input type="text" id="item" name="item" />
+            <input type="text" id="item" name="item"
+            {...register("item", {
+              required: true,
+            })}
+            />
           </div>
           <div className="category">
             <label
@@ -25,6 +31,9 @@ const Rate = () => {
               Select an option
             </label>
             <select
+               {...register("category", {
+                required: true,
+              })}
               id="countries"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
@@ -42,9 +51,14 @@ const Rate = () => {
               type="text"
               id="price"
               name="price"
+              {...register("price", {
+                required: true,
+              })}
             />
           </div>
+          <button type="submit">Submit</button>
         </div>
+        
       </form>
     </>
   );
